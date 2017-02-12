e35ece82c6b377e30c9df50b8a1ff02c4effc3a4
Yassine Echeikh <Yassine Echeikh>
2017-02-11 22:45:44 -0500

    .gitignore is working

--- Server_PC/Assets/Scripts/Boss.cs
+++ Server_PC/Assets/Scripts/Boss.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour {
-
+	EnemyMovementBehaviour myMovementComp;
+	public Vector3 positiveLimit, negativeLimit;
+	public float time;
 	// Use this for initialization
 	void Start () {
-		
+		myMovementComp = GetComponent<EnemyMovementBehaviour> ();
+		myMovementComp.ResetAnimation (positiveLimit, negativeLimit, time);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		myMovementComp.AnimateLoop ();
 	}
 }
