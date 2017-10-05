b1143ae9a1b0737fc24077a562aca5da02795cec
Adrien Logut <newsadrien@gmail.com>
2017-10-04 21:17:22 -0400

    Added socket.io lib

---
+++ Server_PC/Assets/Scripts/Network/SocketB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
+
+public class SocketB : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Socket socket = IO.Socket("http://localhost:3000");
+		socket.On(Socket.EVENT_CONNECT, () =>
+			{
+				socket.Emit("hi", "Hello");
+
+			});
+
+		socket.On("hi", (data) =>
+			{
+				Debug.Log(data);
+				socket.Disconnect();
+			});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
